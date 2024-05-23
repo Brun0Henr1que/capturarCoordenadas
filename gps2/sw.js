05fdef9223c783d94ee4b150af293bf4df0475de
Bruno Henrique Parente de Carvalho <103320427+Brun0Henr1que@users.noreply.github.com>
2024-05-23 20:46:56 -0300

    Primeiro commit

---
+++ gps2/sw.js
@@ -0,0 +1,20 @@
+// Instalação do Service Worker e Cache de recursos
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('gps').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'app.js',
+            ]);
+        })
+    );
+});
+
+// Intercepta solicitações de rede e serve os recursos do cache, se disponí­veis
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
